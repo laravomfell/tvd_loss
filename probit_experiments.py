a42d16007fe0349c77d396e6c4144fb309e29a01
Lara Vomfell <l.vomfell@warwick.ac.uk>
2020-10-21 11:16:58 +0100

    loop through datasets

--- probit_experiments.py
+++ probit_experiments.py
@@ -8,32 +8,31 @@ Created on Wed Sep  2 17:23:25 2020
 Description: File to execute some experiments
 """
 
-from experiment_auxiliaries import get_probit_data, get_test_performance 
-from likelihood_functions import ProbitLikelihood
+from auxiliaries.experiment_auxiliaries import get_data, get_test_performance 
+from npl.likelihood_functions import ProbitLikelihood
+import numpy as np
 
 # set the data path (where is the data stored?)
-data_path = "/Users/jeremiasknoblauch/Documents/OxWaSP/tvd_loss/tvd_loss/data/binary_classification/"
+data_path = "data/probit/"
 
 # set the save path (where are the results stored?)
-save_path = "/Users/jeremiasknoblauch/Documents/OxWaSP/tvd_loss/experiments_new"
-save_path_contam = "/Users/jeremiasknoblauch/Documents/OxWaSP/tvd_loss/experiments_contamination"
+save_path = "data/probit"
 
 # decide which data you want to run the experiments for
-# choices: haberman, fourclass, heart, mammographic_mass, statlog-shuttle, 
-#          breast-cancer-wisconsin
-data_name = "breast-cancer-wisconsin"
+probit_data = ["mammographic_mass", "fourclass", "heart", "haberman", 
+        "breast-cancer-wisconsin"]
 
-
- # get the data
-X, Y = get_probit_data(data_path, data_name)
-
-# set the parameters for the test performance
-num_splits = 50
-train_proportion = 0.9
-L = ProbitLikelihood()
-B = 100
-
-# run the experiments
-get_test_performance(X, Y, num_splits, train_proportion, L, B, 
-                             save_path, data_name)
+for d in probit_data:
+    X, Y = get_data(data_path, d)
+    
+    
+    # set the parameters for the test performance
+    num_splits = 50
+    train_proportion = 0.9
+    L = ProbitLikelihood()
+    B = 100
+    
+    # run the experiments
+    get_test_performance(X, Y, num_splits, train_proportion, L, B, 
+                         save_path, d)
 
